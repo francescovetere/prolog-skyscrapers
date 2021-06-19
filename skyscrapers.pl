3632c89db034bc5edb06ab538f1012fa50a83314
francescovetere <francescovetere1997@gmail.com>
2021-06-19 13:23:46 +0200

    Matrix read from file

--- skyscrapers.pl
+++ skyscrapers.pl
@@ -11,11 +11,8 @@
 %
 % ?- consult('skyscrapers.pl').
 %
-% ?- isMatrixCompliant([[0,0,0,0,0],[0,1,2,3,1],[0,3,1,2,0],[2,2,3,1,0],[0,0,0,3,0]]). 
+% ?- skyscrapers('3x3.txt').
 %		==> true.
-%
-% ?- isMatrixCompliant([[0, 3, 2, 1, 3, 2, 0],[3, 3, 4, 5, 1, 2, 2],[4, 1, 2, 4, 3, 5, 1],[3, 2, 1, 3, 5, 4, 2],[2, 4, 5, 1, 2, 3, 2],[1, 5, 3, 2, 4, 1, 3],[0,1, 2, 4, 2, 4, 0]]). 
-% 	==> true.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -141,11 +138,53 @@ isMatrixCompliant(M) :-
 		removeFirstLast(M, M1), uniqueMatrixFirstLast(M1),        % controllo che all'interno dei bordi vi siano valori unici:
 																															% elimino prima e ultima riga, e controllo se le restanti righe
 																															% contengono valori unici a meno di prima e ultima colonna
-		
+
 		checkRuleForward(M),																			% controllo la regola da sx a dx
 		checkRuleBackward(M),																			% controllo la regola da dx a sx
 
 		transposeMatrix(M, MT),
-		
+
 		checkRuleForward(MT),																			% traspongo e controllo la regola da sx a dx
 		checkRuleBackward(MT).																		% traspongo e controllo la regola da dx a sx
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%% Lettura della matrice da file %%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% leggi_lista_str(L, F) ==> L è la lista letta dal file F
+leggi_lista_str(L, F):- see(F), leggi_lista_str(L), seen.
+leggi_lista_str([S|R]):- leggi_atomo(S), S\==end_of_file, !, leggi_lista_str(R).
+leggi_lista_str([]).
+leggi_atomo(end_of_file):- at_end_of_stream, !.
+leggi_atomo(S):- leggi_lista_car(L), name(S, L).
+leggi_lista_car([C|R]):- get0(C), C\==10, C\==32, C\==(-1), !, leggi_lista_car(R).
+leggi_lista_car([]).
+
+% list_to_matrix(List, N, Matrix) ==> Matrix è la matrice N x N costruita a partire da List
+list_to_matrix([], _, []).
+list_to_matrix(List, Size, [Row|Matrix]):-
+	  list_to_matrix_row(List, Size, Row, Tail),
+  	list_to_matrix(Tail, Size, Matrix).
+
+list_to_matrix_row(Tail, 0, [], Tail).
+list_to_matrix_row([Item|List], Size, [Item|Row], Tail):-
+  	NSize is Size-1,
+  	list_to_matrix_row(List, NSize, Row, Tail).
+
+% build_matrix(File, Matrix) ==> Matrix è la matrice letta dal file File
+build_matrix(File, Matrix) :- leggi_lista_str([H | T], File), list_to_matrix(T, H, Matrix).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%% Predicato principale %%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% skyscrapers(File) ==> true se File contiene una matrice in accordo con le regole del gioco
+skyscrapers(File) :- build_matrix(File, Matrix), isMatrixCompliant(Matrix).
